181ad640cda2bbc17bd3df71391781b0f00d06a3
Chris Domino <cdomino@origindigital.com>
2026-03-22 11:44:41 -0500

    Add subscription ID secret retrieval to infrastructure script

--- Deployment/infrastructure.sh
+++ Deployment/infrastructure.sh
@@ -246,6 +246,7 @@ apiURLResult=$(ensure_key_vault_secret "$keyVaultName" "api-url" "$apiURL");
 tenantIdResult=$(ensure_key_vault_secret "$keyVaultName" "auth-tenant-id" "$tenantId");
 authClientIdResult=$(ensure_key_vault_secret "$keyVaultName" "auth-client-id" "$authClientId");
 apiPrincipalIdResult=$(ensure_key_vault_secret "$keyVaultName" "api-principal-id" "$apiPrincipalId");
+subscriptionIdResult=$(ensure_key_vault_secret "$keyVaultName" "subscription-id", "$subscriptionId");
 authClientSecretResult=$(ensure_key_vault_secret "$keyVaultName" "auth-client-secret" "$authClientSecret");
 sharepointWebhookSecretResult=$(ensure_key_vault_secret "$keyVaultName" "sharepoint-webhook-secret" "$sharepointWebhookSecret");
 
